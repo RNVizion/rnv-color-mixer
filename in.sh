a4eb76ffbfdf6a78b2c1bd66168db6e6995e1a8a
Christian Smith <18348535+RNVizion@users.noreply.github.com>
2026-08-27 09:51:09 -0400

    Update in.sh [skip ci]

--- in.sh
+++ in.sh
@@ -1,3 +1,4 @@
+pip install -r requirements.txt -r tests/requirements-dev.txt
 sudo apt-get update
 sudo apt-get install -y libgl1 libegl1 libxkbcommon-x11-0 libdbus-1-3 \
   libxcb-cursor0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 \
